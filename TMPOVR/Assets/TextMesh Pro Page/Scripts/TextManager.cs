172fca6939b8f639fe74cfe83bb9ea2fda03a4b9
Raihauti <teiki.rai@hotmail.com>
2020-02-21 10:21:11 +0100

    Packages v2.0

---
+++ TMPOVR/Assets/TextMesh Pro Page/Scripts/TextManager.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextManager : MonoBehaviour
+{
+    [Header("Les TextMesh Pro")]
+    /// <summary>
+    /// Le texte à afficher.
+    /// </summary>
+    [Tooltip("Le texte à afficher.")]
+    public TextMeshPro textMesh;
+
+    /// <summary>
+    /// Le texte affichant le numéro de la page.
+    /// </summary>
+    [Tooltip("Le texte affichant le numéro de la page.")]
+    public TextMeshPro textPage;
+
+
+    [Header("Les Boutons")]
+    /// <summary>
+    /// Le bouton allant à la page précédente.
+    /// </summary>
+    [Tooltip("Le bouton allant à la page précédente.")]
+    public SpriteRenderer btnPrecedent;
+
+    /// <summary>
+    /// Le bouton allant à la page suivante.
+    /// </summary>
+    [Tooltip("Le bouton allant à la page suivante.")]
+    public SpriteRenderer btnSuivant;
+
+
+    /// <summary>
+    /// La page courante.
+    /// </summary>
+    private int nbPage;
+
+    /// <summary>
+    /// Les boutons permettant de défiler les pages.
+    /// </summary>
+    private GameObject btnDefilement;
+
+
+
+    private void Start()
+    {
+        btnDefilement = transform.Find("Bouton defilement").gameObject;
+        MajText(textMesh.text);
+    }
+
+    /// <summary>
+    /// Met à jour le numéro de la page.
+    /// </summary>
+    /// <param name="num">Le numéro de la page courante.</param>
+    private void MajPage(int num)
+    {
+        textPage.text = num + "/" + nbPage;
+    }
+
+    /// <summary>
+    /// Met à jour le texte.
+    /// </summary>
+    private void MajText(string txt)
+    {
+        textMesh.text = txt;
+        nbPage = textMesh.GetTextInfo(textMesh.text).pageCount;
+
+        if(nbPage <= 1)
+        {
+            btnDefilement.SetActive(false);
+        }
+
+        else
+        {
+            btnDefilement.SetActive(true);
+            textMesh.pageToDisplay = 1;
+            MajPage(1);
+        }
+    }
+
+    /// <summary>
+    /// Passe à la page suivante.
+    /// </summary>
+    public void Suivant()
+    {
+        if(nbPage > textMesh.pageToDisplay)
+        {
+            textMesh.pageToDisplay++;
+            MajPage(textMesh.pageToDisplay);
+        }
+    }
+
+    /// <summary>
+    /// Retourne à la page précédente.
+    /// </summary>
+    public void Precedent()
+    {
+        if (textMesh.pageToDisplay > 1)
+        {
+            textMesh.pageToDisplay--;
+            MajPage(textMesh.pageToDisplay);
+        }
+    }
+
+    /// <summary>
+    /// Met en surbrillance le sprite.
+    /// </summary>
+    public void In(SpriteRenderer sr)
+    {
+        sr.color = new Color(1, 1, 1, 1f);
+    }
+
+    /// <summary>
+    /// Enlève la surbrillance du sprite.
+    /// </summary>
+    public void Out(SpriteRenderer sr)
+    {
+        sr.color = new Color(1, 1, 1, 0.5f);
+    }
+
+}
---
+++ TMPOVR/Assets/TextMesh Pro Scroll/Scripts/Defilement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Defilement : MonoBehaviour
+{
+    /// <summary>
+    /// Le texte à afficher.
+    /// </summary>
+    [Tooltip("Le texte à afficher")]
+    public TextMeshPro textMesh;
+
+
+    /// <summary>
+    /// Les valeurs de marge du texte.
+    /// </summary>
+    private float left, right, back, top;
+
+    /// <summary>
+    /// L'axe Vertical entre -1 et 1.
+    /// </summary>
+    private float axisVertical;
+
+
+    private void Start()
+    {
+        left = textMesh.margin.x;
+        top = textMesh.margin.y;
+        right = textMesh.margin.z;
+        back = textMesh.margin.w;
+    }
+
+    private void Update()
+    {
+        if (textMesh.color.a == 1)
+        {
+            Scroll();
+        }
+    }
+
+    /// <summary>
+    /// Scrolle le texte.
+    /// </summary>
+    public void Scroll()
+    {
+        axisVertical = Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical");
+        if ((top >= 0 && axisVertical > 0) || (textMesh.isTextOverflowing == false && axisVertical < 0))
+        {
+        }
+        else
+        {
+            top += axisVertical * 2 * Time.deltaTime;
+            textMesh.margin = new Vector4(left, top, right, back);
+        }
+    }
+
+    /// <summary>
+    /// Met en surbrillance le texte.
+    /// </summary>
+    public void In()
+    {
+        textMesh.color = new Color(1, 1, 1, 1);
+    }
+
+    /// <summary>
+    /// Enlève la surbrillance du texte.
+    /// </summary>
+    public void Out()
+    {
+        textMesh.color = new Color(1, 1, 1, 0.5f);
+    }
+}
